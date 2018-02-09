8392488174fb3f968a2a10860eb96cef8df925a1
jademeyer <jadekmeyer@gmail.com>
2018-02-09 00:51:36 -0500

    Create StartPage.swift

---
+++ StartPage.swift
@@ -0,0 +1,35 @@
+//
+//  StartPage.swift
+//  OrchidApp
+//
+//  Created by Jade Meyer on 2/4/18.
+//  Copyright © 2018 Jade Meyer. All rights reserved.
+//
+
+import UIKit
+
+class StartPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
