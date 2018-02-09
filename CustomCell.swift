3b6434c9fde42c760a06b7e1f81e29e726fc8843
jademeyer <jadekmeyer@gmail.com>
2018-02-09 00:53:25 -0500

    Create CustomCell.swift

---
+++ CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  OrchidApp
+//
+//  Created by Jade Meyer on 2/3/18.
+//  Copyright © 2018 Jade Meyer. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+}
