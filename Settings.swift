9a960d5d8581ef58b124a987b67959145029fded
jademeyer <jadekmeyer@gmail.com>
2018-02-22 23:57:47 -0500

    Using Structs

--- Settings.swift
+++ Settings.swift
@@ -13,17 +13,11 @@ class Settings: UIViewController {
     @IBOutlet weak var numPillsSettingsTxtFld: UITextField!
     @IBOutlet weak var numPlacebosSettingsTxtFld: UITextField!
     @IBOutlet weak var numDaysOffSettingsTxtFld: UITextField!
-    @IBOutlet weak var pillAlertSettingsTxtFld: UITextField!
-    @IBOutlet weak var placeboAlertSettingsTxtFld: UITextField!
-    @IBOutlet weak var refillAlertSettingsTxtFld: UITextField!
     
     @IBAction func backTouched(_ sender: Any) {
-        UserDefaults.standard.set(numPillsSettingsTxtFld.text, forKey: "numPills")
-        UserDefaults.standard.set(numPlacebosSettingsTxtFld.text, forKey: "numPlacebos")
-        UserDefaults.standard.set(numDaysOffSettingsTxtFld.text, forKey: "numDaysOff")
-        UserDefaults.standard.set(pillAlertSettingsTxtFld.text, forKey: "pillAlert")
-        UserDefaults.standard.set(placeboAlertSettingsTxtFld.text, forKey: "placeboAlert")
-        UserDefaults.standard.set(refillAlertSettingsTxtFld.text, forKey: "refillAlert")
+        SetUpPage.GlobalVariable.numPills = numPillsSettingsTxtFld.text!
+        SetUpPage.GlobalVariable.numPlacebos = numPlacebosSettingsTxtFld.text!
+        SetUpPage.GlobalVariable.numDaysOff = numDaysOffSettingsTxtFld.text!
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -33,28 +27,9 @@ class Settings: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let numPills = UserDefaults.standard.value(forKey : "numPills") {
-            numPillsSettingsTxtFld.text = numPills as? String
-        }
-        if let numPlacebos = UserDefaults.standard.value(forKey : "numPlacebos") {
-            numPlacebosSettingsTxtFld.text = numPlacebos as? String
-        }
-        if let numDaysOff = UserDefaults.standard.value(forKey : "numDaysOff") {
-            numDaysOffSettingsTxtFld.text = numDaysOff as? String
-        }
-        if let pillAlert = UserDefaults.standard.value(forKey : "pillAlert") {
-            pillAlertSettingsTxtFld.text = pillAlert as? String
-        }
-        if let placeboAlert = UserDefaults.standard.value(forKey : "placeboAlert") {
-            placeboAlertSettingsTxtFld.text = placeboAlert as? String
-        }
-        if let refillAlert = UserDefaults.standard.value(forKey : "refillAlert") {
-            refillAlertSettingsTxtFld.text = refillAlert as? String
-        }
-
-
-
-
+        numPillsSettingsTxtFld.text = SetUpPage.GlobalVariable.numPills
+        numPlacebosSettingsTxtFld.text = SetUpPage.GlobalVariable.numPlacebos
+        numDaysOffSettingsTxtFld.text = SetUpPage.GlobalVariable.numDaysOff
 
         // Do any additional setup after loading the view.
     }
